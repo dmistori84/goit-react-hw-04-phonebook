74bf31a98e9349cd60e406851c5d3bb45cd3d0ea
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-12-19 20:35:17 +0200

    hook useLocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import shortid from 'shortid';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Container } from './Container/Container';
+import { useLocalStorage } from './hooks/useLocalStorage';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(window.localStorage.getItem('contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  });
-  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const [filter, setFilter] = useState('');
 
   const getContacts = data => {
     const newContact = {
@@ -52,7 +46,7 @@ export const App = () => {
   };
 
   const visibleContacts = getVisibleContacts();
-  console.log('filter', filter);
+
   return (
     <Container>
       <h1>Phonebook</h1>
