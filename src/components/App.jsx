3c892b486e6563069fb2d42e06cee98cd78f2c06
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-12-19 20:40:03 +0200

    defaultUser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,15 @@ import { Filter } from './Filter/Filter';
 import { Container } from './Container/Container';
 import { useLocalStorage } from './hooks/useLocalStorage';
 
-export const App = () => {
-  const [contacts, setContacts] = useLocalStorage('contacts', [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+const defaultList = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', defaultList);
   const [filter, setFilter] = useState('');
 
   const getContacts = data => {
